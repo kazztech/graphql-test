a2a337ad793d7a608af0591a082032e3644976ab
kazz <kazzzz1001@gmail.com>
2020-03-06 04:24:23 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # graphql-test
+
+## run
+
+```
+docker-compose up --build
+```
+
+## exec
+
+```
+docker-compose exec app bash
+docker-compose exec db bash
+```
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
+require("dotenv").config();
 var router = express.Router();
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME
+});
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+  con.query("SELECT * FROM users", (err, rows, fields) => {
+    res.send(JSON.stringify(rows));
+  });
 });
 
 module.exports = router;
